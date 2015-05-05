85f07449b17b32f7b5610efff4ae6e16afb852f9
Pharcyde <schorpp@PCBD38047-BS.ad.ti.bund.de>
2015-05-05 15:02:24 +0200

    EndoAdult, wright path.

--- Data/GatherSource/EndoAdult.R
+++ Data/GatherSource/EndoAdult.R
@@ -4,6 +4,8 @@
 # 30.04.2015
 ##############
 
-EndoAdult <- read.delim("~/git_repositories/F2_Earthworms/Data/RW_Alle Aufnahmen.txt")
+Seaparate abundance data of endogeic earthworms into adult and juveniles
+
+EndoAdult <- read.delim("~/git_repositories/F2_Earthworms/Data/F2_EW_EndoAdult.txt")
 
 data$endad <- rowSums(EndoAdult[,c("AROad", "ACAad", "ACHad", "ENDad","OCY","OLA")])
